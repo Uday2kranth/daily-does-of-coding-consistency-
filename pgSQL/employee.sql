4c1496fa60b2f818bb140fb8c5be0331601c9ef5
Uday2kranth <udaykranth01@gmail.com>
2026-01-20 02:59:54 +0530

    employee shecma creation and insertion data query for reference

---
+++ pgSQL/employee.sql
@@ -0,0 +1,10 @@
+create table employee(
+id int,
+first_name varchar(50),
+last_name varchar(50)
+);
+insert into employee( id , first_name ,last_name) values ( 1, "UdayKranth", "Pendyala")
+insert into employee( id , first_name ,last_name) values ( 2, "GaganKranthi", "Yanamanagandla",
+3,"SaiKiran")
+
+select * from employee;
