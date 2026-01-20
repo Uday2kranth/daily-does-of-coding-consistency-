36fa586a71d774f2c1d3c9577b721a7170c53023
Pendyala Uday Kranth <udaykranth01@gmail.com>
2026-01-20 08:37:36 +0530

    Insert multiple employee records into employee table
    Added multiple employee records to the database.

--- pgSQL/employee.sql
+++ pgSQL/employee.sql
@@ -3,8 +3,12 @@ id int,
 first_name varchar(50),
 last_name varchar(50)
 );
-insert into employee( id , first_name ,last_name) values ( 1, "UdayKranth", "Pendyala")
-insert into employee( id , first_name ,last_name) values ( 2, "GaganKranthi", "Yanamanagandla",
-3,"SaiKiran")
+insert into employee( id , first_name ,last_name) values ( 1, 'UdayKranth', 'Pendyala')
+insert into employee( id , first_name ,last_name) values ( 2, 'GaganKranthi', 'Yanamanagandla'),
+(3,'SaiKiran','Choudari'),
+(4,'SaiRam','s'),
+(6,'Akash','D'),
+(5,'Tharun','Bodula'),
+(7,'BhanuPrakash','G');
 
 select * from employee;
